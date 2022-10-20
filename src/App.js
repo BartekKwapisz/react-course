cf91609791b249f6fd3f42b64ecab07700cc0d8d
Bartek Kwapisz <kwapiszbartek@gmail.com>
2022-10-21 00:13:43 +0200

    just following the course

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import Todo from "./components/Todo";
+
 function App() {
-  return <div>Hello</div>;
+  return (
+    <div>
+      <h1>My Todos</h1>
+      <Todo text='learn React'/>
+      <Todo text='kfdkkd'/>
+      <Todo text='powepowepopew'/>
+    </div>
+  );
 }
 
 export default App;
